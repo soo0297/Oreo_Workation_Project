0140225c6b44f95f096b43afa48d7b52d150511e
byoungyup_kim <qudduq999@daum.net>
2024-08-29 19:09:22 +0900

    ✨: 공통 모달 컴포넌트 업데이트

---
+++ src/components/custom/useModal.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useModal = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return { isModalOpen, toggleModal };
+};
+
+export default useModal;
